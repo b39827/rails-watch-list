4d90756fa7c59669d5ea88fd038419936f7b0852
bp <brunnopg@me.com>
2023-11-17 17:06:21 +0800

    done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,32 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @movies = @list.movies
+    @bookmark = Bookmark.new
+    @bookmarks = Bookmark.where(list_id: params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to @list
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
